ad6683156bf4cd9aa4f91dfb3ebc005f6662323e
aravind-25-gopinath <aravindgopinath25@gmail.com>
2021-05-10 12:25:26 -0400

    Initial

--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import "fmt"
 
 func main() {
 	//var card string = "Ace of Spades" //more explicit of way telling Go that 'card' is a String variable
-	card := "Ace of Spades" //this lets Go infer the data type of the variable (:= only used when we FIRST instantiate a variable)
+	card := newCard() //this lets Go infer the data type of the variable (:= only used when we FIRST instantiate a variable)
 	fmt.Println(card)
+}
+
+//function that returns type string ("Five of Diamonds")
+func newCard() string {
+	return "Five of Diamonds"
 }
