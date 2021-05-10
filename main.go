d1293ca242c58592fbfb5af622409db643befbf0
aravind-25-gopinath <aravindgopinath25@gmail.com>
2021-05-10 12:56:20 -0400

    Reciever Function

--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 
-import "fmt"
-
 func main() {
 	//var card string = "Ace of Spades" //more explicit of way telling Go that 'card' is a String variable
 	/*card := newCard() //this lets Go infer the data type of the variable (:= only used when we FIRST instantiate a variable)
 	fmt.Println(card)*/
 
-	cards := []string{newCard(), newCard()} //a slice of 2 string elements
-	cards = append(cards, "Six of Spades")  //adds "Six of Spades" to the slice
+	//cards := []string{newCard(), newCard()} //a slice of 2 string elements
+	cards := deck{"Ace of Diamonds", newCard()} //deck is something that is initialized in deck.go, cards is of type deck
+	cards = append(cards, "Six of Spades")      //adds "Six of Spades" to the slice
 
 	//iterate over the cards slice
-	for i, cards := range cards {
-		fmt.Println(i, cards)
+	/*for i, card := range cards {
+		fmt.Println(i, card)
 	}
+
+	//another way of iterating over the cards
+	for i := 0; i < len(cards); i++ {
+		fmt.Println(i, cards[i])
+	}*/
+
+	cards.print() //reciever function from deck.go
 }
 
 //function that returns type string ("Five of Diamonds")
